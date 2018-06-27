a454ee7ddd8dd5a90ee2f0724a3acea37f74bda8
Ordanis Sanchez <ordanisanchez@gmail.com>
2018-06-27 15:23:13 -0400

    Add request params samples, add type notations

--- api.py
+++ api.py
@@ -1,24 +1,34 @@
 from sanic import Sanic
-from sanic import response
+from sanic.response import text, json, html
 
 app = Sanic(__name__)
 
 
 @app.route('/async')
-async def index(request):
-    # return response.text('hi')
-    return response.json({
-        'hello': 'PyDO'
-    })
+async def index(request) -> json:
+    return json({'hello': 'PyDO'})
 
 
 @app.route('/noasync')
-def noasync(request):
-    # return response.text('No Async')
-    return response.json({
-        'hello': 'PyDO2'
-    })
+def noasync(request) -> text:
+    return text('Hello PyDO')
 
+
+@app.route('/hello/<name>')
+async def hello(request, name: str) -> json:
+    return json({'hello': name})
+
+
+@app.route('/age/<age:int>')
+async def age(request, age: int) -> json:
+    return json({'hello': age})
+
+
+async def phrase(request, phrase: str) -> html:
+    return html(f'<p>Hey say something: <strong>{phrase}</strong></p>')
+
+
+app.add_route(phrase, '/phrase', methods=['POST'])
 
 if __name__ == '__main__':
     app.run(port=5000)
