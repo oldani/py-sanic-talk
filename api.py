8685913f217d2310948bdef16fff4504f6829553
Ordanis Sanchez <ordanisanchez@gmail.com>
2018-06-27 15:07:02 -0400

    Add json response

--- api.py
+++ api.py
@@ -4,14 +4,20 @@ from sanic import response
 app = Sanic(__name__)
 
 
-@app.route('/')
+@app.route('/async')
 async def index(request):
-    return response.text('hola')
+    # return response.text('hi')
+    return response.json({
+        'hello': 'PyDO'
+    })
 
 
 @app.route('/noasync')
 def noasync(request):
-    return response.text('No Async')
+    # return response.text('No Async')
+    return response.json({
+        'hello': 'PyDO2'
+    })
 
 
 if __name__ == '__main__':
